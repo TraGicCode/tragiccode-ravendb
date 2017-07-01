2c9661dd8174daa5f17608e379e51dd1a7464db6
tragiccode <michael@tragiccode.com>
2017-07-01 17:32:29 -0500

    Should download installer and install ravnedb

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,20 @@
 class ravendb::install(
   Enum['installed', 'present', 'absent'] $package_ensure = $ravendb::params::package_ensure,
 ) inherits ravendb::params {
-  # resources
 
+  $file_ensure = $package_ensure ? {
+    'installed' => 'file',
+    'present'   => 'file',
+    default     => 'absent',
+  }
+  file { $ravendb::params::ravendb_download_absolute_path:
+    ensure => $file_ensure,
+    source => $ravendb::params::ravendb_download_url,
+  }
+
+  package { 'RavenDB':
+    ensure => $package_ensure,
+    source => $ravendb::params::ravendb_download_absolute_path,
+  }
 
 }
--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -2,6 +2,34 @@ require 'spec_helper'
 describe 'ravendb::install' do
   context 'with default values for all parameters' do
     it { should contain_class('ravendb::install') }
-    it { should contain_class('ravendb::params')}
+    it { should contain_class('ravendb::params') }
+
+    it { should contain_file('C:\\RavenDB-3.5.3.Setup.msi').with({
+      :ensure => 'file',
+      :source => 'https://daily-builds.s3.amazonaws.com/RavenDB-3.5.3.Setup.exe',
+    }) }
+
+    it { should contain_package('RavenDB').with({
+      :ensure => 'present',
+      :source => 'C:\\RavenDB-3.5.3.Setup.msi',
+    }).that_requires('File[C:\\RavenDB-3.5.3.Setup.msi]')}
+
+  end
+
+  context 'with package_ensure => absent' do
+    let(:params) {{
+      :package_ensure => 'absent',
+    }}
+
+    it { should contain_file('C:\\RavenDB-3.5.3.Setup.msi').with({
+      :ensure => 'absent',
+      :source => 'https://daily-builds.s3.amazonaws.com/RavenDB-3.5.3.Setup.exe',
+    }) }
+    
+    it { should contain_package('RavenDB').with({
+      :ensure => 'absent',
+      :source => 'C:\\RavenDB-3.5.3.Setup.msi',
+    }).that_requires('File[C:\\RavenDB-3.5.3.Setup.msi]')}
+
   end
 end
