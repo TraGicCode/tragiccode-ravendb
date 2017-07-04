74aba105e56ffe7d94267853bdbbf327576990c6
tragiccode <michael@tragiccode.com>
2017-07-04 12:21:20 -0500

    Added the ability to uninstall ravendb

--- spec/acceptance/ravendb_installation_spec.rb
+++ spec/acceptance/ravendb_installation_spec.rb
@@ -25,6 +25,10 @@ describe 'ravendb' do
        it { should exist }
     end
 
+    describe package('RavenDB') do
+      it { should be_installed }
+    end
+
     describe port(8080) do
       it { should be_listening }
     end
@@ -33,6 +37,45 @@ describe 'ravendb' do
       it { should be_installed }
       it { should be_running }
       it { should be_enabled }
+    end
+  end
+
+
+    context 'when uninstalling with provided mandatory parameters' do
+    let(:install_manifest) {
+      <<-MANIFEST
+          class { 'ravendb':
+              package_ensure       => 'absent',
+              ravendb_service_name => 'RavenDB',
+              ravendb_port         => 8080,
+          }
+        MANIFEST
+    }
+
+    it 'should run without errors' do
+      apply_manifest(install_manifest, :catch_failures => true)
+    end
+
+    it 'should be idempotent' do
+      apply_manifest(install_manifest, :catch_changes => true)
+    end
+
+    describe file('C:\RavenDB-3.5.3.Setup.exe') do
+       it { should_not exist }
+    end
+
+    describe package('RavenDB') do
+      it { should_not be_installed }
+    end
+
+    describe port(8080) do
+      it { should_not be_listening }
+    end
+
+    describe service('RavenDB') do
+      it { should_not be_installed }
+      it { should_not be_running }
+      it { should_not be_enabled }
     end
   end
 
