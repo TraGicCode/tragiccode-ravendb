81715ebbd75bf96211cfd8418b2e9319367a7c9b
tragiccode <michael@tragiccode.com>
2017-07-05 15:54:51 -0500

    Fix beaker manifest

--- spec/acceptance/ravendb_installation_spec.rb
+++ spec/acceptance/ravendb_installation_spec.rb
@@ -10,7 +10,7 @@ describe 'ravendb' do
               ravendb_service_name                   => 'RavenDB',
               ravendb_port                           => 8080,
               ravendb_install_log_absolute_path      => 'C:\\RavenDB.install.log',
-              ravendb_database_directory             => 'C:\\RavenDB\\Databases'
+              ravendb_database_directory             => 'C:\\RavenDB\\Databases',
               ravendb_filesystems_database_directory => 'C:\\RavenDB\\FileSystems',
           }
         MANIFEST
