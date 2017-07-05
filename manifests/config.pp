403aff1dcd43bc6764f2622749c4e1515c0bd40c
tragiccode <michael@tragiccode.com>
2017-07-05 17:40:58 -0500

    Place appropriate carriage return and newlines in ravenserver.exe.config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,7 +12,7 @@ class ravendb::config(
 
   file { $ravendb_server_exe_config_absolute_path:
     ensure  => file,
-    content => epp('ravendb/Raven.Server.exe.config.epp', $raven_server_exe_config_hash),
+    content => regsubst(epp('ravendb/Raven.Server.exe.config.epp', $raven_server_exe_config_hash), '\n', "\r\n", 'EMG'),
   }
 
 }
