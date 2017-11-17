614132d9f6e02fca585b3bfa72ba5af3d28d4b52
tragiccode <6224270+TraGicCode@users.noreply.github.com>
2017-11-17 17:24:45 -0600

    Moved hardcoded defaults in template to a defaults hash in the code to allow for handling duplicates

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -7,7 +7,16 @@ describe 'ravendb::config' do
 
     it {
       is_expected.to contain_file('C:\\RavenDB\Raven.Server.exe.config').with(ensure: 'file')
-                                                                        .with_content(%r{#{Regexp.escape('<add key="Raven/Port" value="8080"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/Port" value="8080"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/DataDir/Legacy" value="~\Database\System"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/DataDir" value="~\Databases\System"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/AnonymousAccess" value="Admin"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/Licensing/AllowAdminAnonymousAccessForCommercialUse" value="false"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/AccessControlAllowOrigin" value="*"/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/IndexStoragePath" value=""/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/TransactionJournalsPath" value=""/>')}})
+        .with_content(%r{#{Regexp.escape('<add key="Raven/WorkingDir" value="~\"/>')}})
+                                                                        .with_content(%r{#{Regexp.escape('<add key="Raven/FileSystem/DataDir" value="~\FileSystems"/>')}})
     }
   end
 
