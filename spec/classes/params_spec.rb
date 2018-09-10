4ac52f0602ea690ffd327f508442df1394b3766b
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-09-10 00:10:53 -0500

    Clean slate

--- spec/classes/params_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'ravendb::params' do
-  context 'with default values for all parameters' do
-    it { is_expected.to contain_class('ravendb::params') }
-    # This is a params class....
-    it { is_expected.to have_resource_count(0) }
-  end
-end
